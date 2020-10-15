89cb6079de60310af9855bf4cba6feedc17a7111
Justus Soh <45728710+justussoh@users.noreply.github.com>
2020-10-15 18:01:11 +0800

    Feat/local resolver (#2)
    * fix: export types
    
    * feat: combined handling of csv file

--- src/common/hooks/useAddressBook.tsx
+++ src/common/hooks/useAddressBook.tsx
@@ -1,17 +1,28 @@
 import { useCallback, SetStateAction, Dispatch } from "react";
 import createPersistedState from "use-persisted-state";
 import { AddressBookLocalProps, ResolutionResult } from "../../types";
+import { readAsText, csvToAddressBook } from "../../services/localAddressBookResolver";
 
 interface UseAddressBook {
   addressBook: AddressBookLocalProps;
   setAddressBook: Dispatch<SetStateAction<AddressBookLocalProps>>;
-  getIdentifier: (arg0: string) => ResolutionResult | undefined;
+  handleLocalAddressBookCsv: (csvFile: File) => Promise<void>;
+  getIdentifier: (address: string) => ResolutionResult | undefined;
 }
 
 // add return types
 export const useAddressBook = (): UseAddressBook => {
   const defaultAddressBook: AddressBookLocalProps = {};
   const [addressBook, setAddressBook] = createPersistedState("ADDRESS_BOOK")(defaultAddressBook);
+  const handleLocalAddressBookCsv = useCallback(
+    async (csvFile: File): Promise<void> => {
+      const csv = await readAsText(csvFile);
+      const addressBook = csvToAddressBook(csv);
+      setAddressBook(addressBook);
+    },
+    [setAddressBook]
+  ); // useCallback needed to prevent multiple calls
+
   const getIdentifier = useCallback(
     (address: string): ResolutionResult | undefined => {
       const result = addressBook[address.toLowerCase()];
@@ -19,5 +30,5 @@ export const useAddressBook = (): UseAddressBook => {
     },
     [addressBook]
   ); // useCallback needed to prevent multiple calls
-  return { addressBook, setAddressBook, getIdentifier };
+  return { addressBook, setAddressBook, handleLocalAddressBookCsv, getIdentifier };
 };
